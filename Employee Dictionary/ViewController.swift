108d8795c453d2dffc173689a41d844fefa0d37b
Gary_Hsu <chun52117@gmail.com>
2022-08-13 18:02:16 -0700

    Create project "Employee-Directory"
    [what] Create in Xcode 13.4.1 / Apple Swift version 5.4.2
    [why]
    [how]
    [risk]
    Auditor:

---
+++ Employee Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Employee Dictionary
+//
+//  Created by Yu-Chun Hsu on 2022/8/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Employee DictionaryUITests/Employee_DictionaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Employee_DictionaryUITestsLaunchTests.swift
+//  Employee DictionaryUITests
+//
+//  Created by Yu-Chun Hsu on 2022/8/13.
+//
+
+import XCTest
+
+class Employee_DictionaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
