df9ee7a614dadf4cd354185871ff9e249d618035
Gary_Hsu <chun52117@gmail.com>
2022-09-20 18:43:06 -0700

    Refine Code: Create view in ViewController.swift instead of EmployeeDictionaryView.swift.
    [what]
    [why] It's not necessary to separate the view and viewController.
    [how]
    [risk]
    Auditor:

--- Employee Dictionary/ViewController/ViewController.swift
+++ Employee Dictionary/ViewController/ViewController.swift
@@ -16,22 +16,50 @@ class ViewController: UIViewController {
     var groupedEmployeesData:[(String.Element?,[Employee])]?
     var rawGroupedEmployeesData:[(String.Element?,[Employee])]?
     
-    private var customView: EmployeeDictionaryView = {
-        var view = EmployeeDictionaryView.init(frame: UIScreen.main.bounds)
+    var navigationBar: UINavigationBar = {
+        var view = UINavigationBar()
         return view
     }()
     
-    override func loadView() {
-        self.view = customView
-    }
+    var searchBar: UISearchBar = {
+        var view = UISearchBar()
+        view.placeholder = "Search employee"
+        view.textContentType = .name
+        view.searchBarStyle = .minimal
+        return view
+    }()
+    
+    var plainView: UIView = {
+        var view = UIView()
+        view.backgroundColor = .systemBackground
+        view.isHidden = true
+        return view
+    }()
+    
+    var indicatorView: UIActivityIndicatorView = {
+        var view = UIActivityIndicatorView()
+        view.hidesWhenStopped = true
+        return view
+    }()
+    
+    var tableView: UITableView = {
+        var view = UITableView()
+        view.backgroundColor = .red
+        view.register(EmployeeInfoTableViewCell.self, forCellReuseIdentifier: "EmployeeInfoTableViewCell")
+        view.showsVerticalScrollIndicator = false
+        return view;
+    }()
+    
+    var searchFilterClosure:((_ searchText: String) -> ())?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configUI()
         
-        self.customView.tableView.dataSource = self
-        self.customView.tableView.delegate = self
-        self.customView.searchFilterClosure = { [weak self] searchText in
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.searchFilterClosure = { [weak self] searchText in
             self?.filterEmployeesData(with: searchText)
         }
         
@@ -57,9 +85,50 @@ class ViewController: UIViewController {
             self.rawGroupedEmployeesData = groupedEmployees
             
             DispatchQueue.main.async {
-                self.customView.tableView.reloadData()
+                self.tableView.reloadData()
             }
         })
+    }
+    
+    func configUI() {
+        self.view.backgroundColor = .systemBackground
+       
+        self.view.addSubview(self.navigationBar)
+        self.navigationBar.translatesAutoresizingMaskIntoConstraints = false
+        self.navigationBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.navigationBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.navigationBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        self.navigationBar.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        
+        let titleItem = UINavigationItem(title: NSLocalizedString("Employee Directory", comment: ""))
+        self.navigationBar.setItems([titleItem], animated: false)
+        
+        self.view.addSubview(self.searchBar)
+        self.searchBar.translatesAutoresizingMaskIntoConstraints = false
+        self.searchBar.topAnchor.constraint(equalTo: self.navigationBar.bottomAnchor, constant: 13).isActive = true
+        self.searchBar.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        self.searchBar.heightAnchor.constraint(equalToConstant: 37).isActive = true
+        self.searchBar.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.83).isActive = true
+        self.searchBar.delegate = self
+        
+        self.view.addSubview(self.tableView)
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.topAnchor.constraint(equalTo: self.searchBar.bottomAnchor, constant: 10).isActive = true
+        self.tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 12).isActive = true
+        self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+        
+        self.view.addSubview(self.plainView)
+        self.plainView.translatesAutoresizingMaskIntoConstraints = false
+        self.plainView.topAnchor.constraint(equalTo: self.searchBar.bottomAnchor, constant: 10).isActive = true
+        self.plainView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        self.plainView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 12).isActive = true
+        self.plainView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+        
+        self.plainView.addSubview(self.indicatorView)
+        self.indicatorView.translatesAutoresizingMaskIntoConstraints = false
+        self.indicatorView.centerYAnchor.constraint(equalTo: self.plainView.centerYAnchor).isActive = true
+        self.indicatorView.centerXAnchor.constraint(equalTo: self.plainView.centerXAnchor).isActive = true
     }
     
     func filterEmployeesData(with searchText: String) {
@@ -83,7 +152,7 @@ class ViewController: UIViewController {
                 }
             }
         }
-        self.customView.tableView.reloadData()
+        self.tableView.reloadData()
     }
     
 }
@@ -140,8 +209,8 @@ extension ViewController: UITableViewDataSource {
         let employeeProfileViewModel = EmployeeProfileViewModel(employeeModel: model)
         
         employeeDetailVC.employeeProfileViewModel = employeeProfileViewModel
-        self.customView.searchBar.resignFirstResponder()
-        if let cancelButton = self.customView.searchBar.value(forKey: "cancelButton") as? UIButton {
+        self.searchBar.resignFirstResponder()
+        if let cancelButton = self.searchBar.value(forKey: "cancelButton") as? UIButton {
             cancelButton.isEnabled = true
         }
         self.navigationController?.present(employeeDetailVC, animated: true)
@@ -161,11 +230,37 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UITableViewDelegate, UIScrollViewDelegate {
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        self.customView.searchBar.resignFirstResponder()
-        if let cancelButton = self.customView.searchBar.value(forKey: "cancelButton") as? UIButton {
+        self.searchBar.resignFirstResponder()
+        if let cancelButton = self.searchBar.value(forKey: "cancelButton") as? UIButton {
             cancelButton.isEnabled = true
         }
         
     }
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // TODO: Implement Search Function
+        guard let searchFilterClosure = self.searchFilterClosure else {
+            return
+        }
+        searchFilterClosure(searchText)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.setShowsCancelButton(false, animated: true)
+        self.searchBar.resignFirstResponder()
+    }
+}
